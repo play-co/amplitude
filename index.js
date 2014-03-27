ef9045ba5209376b2fa98a46c6efa2c3ac70884a
Chris A. Taylor <chris@gameclosure.com>
2014-03-27 11:17:03 -0700

    submod

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 exports.init = function (common) {
-	common.child("git", "submodule update --init --recursive");
+	var logger = new common.Formatter('amplitude');
+
+	logger.log("Pulling in the Amplitude SDK");
+
+	common.child("git", ["submodule", "update", "--init", "--recursive"], {cwd: __dirname}, function() {
+		logger.log("Done updating the Amplitude SDK");
+	});
 };
 
 exports.load = function (common) {
